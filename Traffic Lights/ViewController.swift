35041bb928d46ea941436c570473384665d3efc7
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-09-21 16:55:37 +0300

    Set button and traffic lights some action.

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -2,18 +2,64 @@
 //  ViewController.swift
 //  Traffic Lights
 //
-//  Created by Alexey Elfimov on 20.09.2024.
+//  Created by Alexey Yelfimov on 20.09.2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    enum TrafficLightState {
+        case off, red, yellow, green
+    }
+    
+    var currentLightState: TrafficLightState = .off
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    
+    @IBAction func switchButtonPressed(_ sender: UIButton) {
+        switch currentLightState {
+        case .off:
+            currentLightState = .red
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            startButton.setTitle("NEXT", for: .normal)
+        case .red:
+            currentLightState = .yellow
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        case .yellow:
+            currentLightState = .green
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        case .green:
+            currentLightState = .red
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        }
+    }
+    
 }
-
