475052b24b24ca785fa71bb35674ddf6714a5203
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-09-21 22:00:37 +0300

    Fixed the "unrecognized selector sent to instance" bug.

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
@@ -24,6 +23,8 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startButton.layer.cornerRadius = 10
+        
         redLight.alpha = 0.3
         yellowLight.alpha = 0.3
         greenLight.alpha = 0.3
@@ -31,35 +32,35 @@ final class ViewController: UIViewController {
         redLight.layer.cornerRadius = redLight.frame.size.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
-        
-        startButton.layer.cornerRadius = 10
     }
     
-    
-    @IBAction func switchButtonPressed(_ sender: UIButton) {
-        switch currentLightState {
+  
+
+    @IBAction func lightButtonSwitcher() {
+    switch currentLightState {
         case .off:
-            currentLightState = .red
-            redLight.alpha = 1
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 0.3
-            startButton.setTitle("NEXT", for: .normal)
-        case .red:
-            currentLightState = .yellow
-            redLight.alpha = 0.3
-            yellowLight.alpha = 1
-            greenLight.alpha = 0.3
-        case .yellow:
-            currentLightState = .green
-            redLight.alpha = 0.3
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 1
-        case .green:
-            currentLightState = .red
-            redLight.alpha = 1
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 0.3
+                currentLightState = .red
+                redLight.alpha = 1
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 0.3
+                startButton.setTitle("NEXT", for: .normal)
+            case .red:
+                currentLightState = .yellow
+                redLight.alpha = 0.3
+                yellowLight.alpha = 1
+                greenLight.alpha = 0.3
+            case .yellow:
+                currentLightState = .green
+                redLight.alpha = 0.3
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 1
+                startButton.setTitle("START", for: .normal)
+            case .green:
+                currentLightState = .red
+                redLight.alpha = 1
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 0.3
+                startButton.setTitle("NEXT", for: .normal)
         }
     }
-    
 }
