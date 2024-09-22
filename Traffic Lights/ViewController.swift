a9c918b7f9edd5bbdf04ccecb86005c31b4636dd
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-09-22 23:29:26 +0300

    Fixed the code issues.

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,60 +8,55 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var startButton: UIButton!
     
-    enum TrafficLightState {
-        case off, red, yellow, green
-    }
-    
-    var currentLightState: TrafficLightState = .off
+    private var currentLight = CurrentLight.red
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startButton.layer.cornerRadius = 10
         
-        redLight.alpha = 0.3
-        yellowLight.alpha = 0.3
-        greenLight.alpha = 0.3
-        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+    }
+
+    override func viewWillLayoutSubviews() {
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
     
-  
-
     @IBAction func lightButtonSwitcher() {
-    switch currentLightState {
-        case .off:
-                currentLightState = .red
-                redLight.alpha = 1
-                yellowLight.alpha = 0.3
-                greenLight.alpha = 0.3
-                startButton.setTitle("NEXT", for: .normal)
-            case .red:
-                currentLightState = .yellow
-                redLight.alpha = 0.3
-                yellowLight.alpha = 1
-                greenLight.alpha = 0.3
-            case .yellow:
-                currentLightState = .green
-                redLight.alpha = 0.3
-                yellowLight.alpha = 0.3
-                greenLight.alpha = 1
-                startButton.setTitle("START", for: .normal)
-            case .green:
-                currentLightState = .red
-                redLight.alpha = 1
-                yellowLight.alpha = 0.3
-                greenLight.alpha = 0.3
-                startButton.setTitle("NEXT", for: .normal)
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
         }
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+}
+
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
     }
 }
